c68ec16c3f6d4490e4a440cbb46873cd571b5000
jking323 <43685815+jking323@users.noreply.github.com>
2020-11-29 22:44:14 -0500

    testing wsl setup with basic hello world, this will change in next commit

--- kernel.c
+++ kernel.c
@@ -3,6 +3,11 @@ Author: Jeremy King
 Version: 0.01a
  11/24/2020
  */
+/*TODO
+ Write initial kernel
+ Learn to spell kernel correctly!!
+ Figure out how to get interactive keyboard support
+ */
 #include <stdio.h>
 
 int main()
